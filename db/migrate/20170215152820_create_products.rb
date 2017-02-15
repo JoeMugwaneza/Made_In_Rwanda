ceed69de4fe9cfff1054e506c2a0ff4d1b6cde30
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-15 17:58:20 +0200

    initial commit

---
+++ db/migrate/20170215152820_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :image
+      t.text :description
+      t.decimal :price, precision: 9, scale: 2
+
+      t.timestamps
+    end
+  end
+end
