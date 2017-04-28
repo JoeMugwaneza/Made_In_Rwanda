cc1ad97dbaa616320647d1eb61db8a1e895755ed
JoeMugwaneza <muganezajoseph@gmail.com>
2017-04-28 16:03:47 +0200

    keep it secretly

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'dotenv-rails', '~> 2.2', '>= 2.2.1'
 gem 'rails', '~> 5.0.1'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'bootswatch-rails'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               '0.0.0.0:3000',
+      user_name:            ENV['EMAIL'],
+      password:             ENV['PASSWORD'],
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
+
+ActionMailer::Base.default_content_type = "text/html"
