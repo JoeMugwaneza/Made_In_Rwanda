cb4e12dd4b5336d93ca6acb7993d348d05b64d86
JoeMugwaneza <muganezajoseph@gmail.com>
2017-09-19 19:11:23 +0200

    faked migration

--- db/migrate/20170919170517_add_column_to_users.rb
+++ db/migrate/20170919170517_add_column_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :users, :name, :first_name
-    add_column :users, :last_name, :string
-  end
+  # def change
+  #   rename_column :users, :name, :first_name
+  #   add_column :users, :last_name, :string
+  # end
 end
